921c612dbb8ad51b482e514c8252948647a368f2
Kien Nguyen <kien@caliber.build>
2024-08-21 13:58:31 +0700

    BENCHMARKALLATONCE

--- benches/mainnet.rs
+++ benches/mainnet.rs
@@ -4,7 +4,7 @@
 
 #![allow(missing_docs)]
 
-use std::{num::NonZeroUsize, thread};
+use std::num::NonZeroUsize;
 
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use pevm::chain::PevmEthereum;
@@ -18,12 +18,6 @@ static GLOBAL: rpmalloc::RpMalloc = rpmalloc::RpMalloc;
 
 pub fn criterion_benchmark(c: &mut Criterion) {
     let chain = PevmEthereum::mainnet();
-    let concurrency_level = thread::available_parallelism()
-        .unwrap_or(NonZeroUsize::MIN)
-        // 8 seems to be the sweet max for Ethereum blocks. Any more
-        // will yield many overheads and hurt execution on (small) blocks
-        // with many dependencies.
-        .min(NonZeroUsize::new(8).unwrap());
 
     common::for_each_block_from_disk(|block, storage| {
         let mut group = c.benchmark_group(format!(
@@ -32,28 +26,37 @@ pub fn criterion_benchmark(c: &mut Criterion) {
             block.transactions.len(),
             block.header.gas_used
         ));
-        group.bench_function("Sequential", |b| {
+        group.warm_up_time(std::time::Duration::from_millis(100));
+        group.sample_size(10);
+        group.measurement_time(std::time::Duration::from_secs(1));
+        group.sampling_mode(criterion::SamplingMode::Flat);
+
+        group.bench_function("S", |b| {
             b.iter(|| {
                 pevm::execute(
                     black_box(&storage),
                     black_box(&chain),
                     black_box(block.clone()),
-                    black_box(concurrency_level),
+                    black_box(NonZeroUsize::MIN),
                     black_box(true),
                 )
             })
         });
-        group.bench_function("Parallel", |b| {
-            b.iter(|| {
-                pevm::execute(
-                    black_box(&storage),
-                    black_box(&chain),
-                    black_box(block.clone()),
-                    black_box(concurrency_level),
-                    black_box(false),
-                )
-            })
-        });
+
+        for concurrency_level in 4..=16 {
+            group.bench_function(format!("P{concurrency_level}"), |b| {
+                b.iter(|| {
+                    pevm::execute(
+                        black_box(&storage),
+                        black_box(&chain),
+                        black_box(block.clone()),
+                        black_box(NonZeroUsize::new(concurrency_level).unwrap()),
+                        black_box(false),
+                    )
+                })
+            });
+        }
+
         group.finish();
     });
 }
