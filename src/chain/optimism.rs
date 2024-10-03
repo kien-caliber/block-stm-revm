d7fa2c62305950099edcd5e4ae3e517ebbebe843
Kien Nguyen <kien@caliber.build>
2024-10-03 22:31:22 +0700

    feat: allow custom chain id

--- src/chain/optimism.rs
+++ src/chain/optimism.rs
@@ -29,6 +29,11 @@ impl PevmOptimism {
             id: NamedChain::Optimism.into(),
         }
     }
+
+    /// Custom network
+    pub fn custom(id: u64) -> Self {
+        PevmOptimism { id }
+    }
 }
 
 #[derive(Debug, Clone, PartialEq)]
